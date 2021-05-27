fdb8b25919c3cf37e5f11f0b57e2c6bb9c69313b
rewpt <zpermack@gmail.com>
2021-05-27 14:00:30 -0400

    Added Buttons for Random Colors 1 and 2 as well as Lucky Dip
    - Random Color 1 button selects random color for first gradient
    - Random Color 2 button selects random color for seconds gradient
    - Lucky dip random selects colors 1 and 2
    Stylings for buttons still need to be done.

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
-var body = document.querySelector("body") 
+var body = document.querySelector("body");
+var randColor1 = document.querySelector(".randColor1");
+var randColor2 = document.querySelector(".randColor2");
+var luckyDip = document.querySelector(".luckyDip");
 
 //Using CSS style in JS to change the background
 const gradientChange = () => {
   body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value}`;
   css.textContent = body.style.background + ";"
+}
+//Random Color picker Fx
+function generateRandomColor()
+{
+    var randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+    return randomColor;
+}
+function randomColor1() {
+  color1.value = generateRandomColor()
+  gradientChange()
+}
+function randomColor2(){
+  color2.value = generateRandomColor()
+  gradientChange()
+}
+function luckyDipFx(){
+  randomColor1();
+  randomColor2();
 }
 
 //Using an event listener of input to instantly record the change in the color picker
@@ -15,6 +36,21 @@ const gradientChange = () => {
 color1.addEventListener("input", gradientChange)
 //Same thing but with color2
 color2.addEventListener("input", gradientChange)
+//Random Color 1 click listener
+randColor1.addEventListener("click", () =>{
+  randomColor1();
+}
+)
+//Random Color 2 click listener
+randColor2.addEventListener("click", () =>{
+  randomColor2();
+}
+)
+//Lucky Dip click listener
+luckyDip.addEventListener("click", () => {
+  luckyDipFx();
+})
+
 
 
 
